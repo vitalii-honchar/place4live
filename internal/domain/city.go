7c763114d7de1981ac817d76365dab8f794e4a05
Vitalii Honchar <weaxme@gmail.com>
2022-12-28 14:14:24 -0500

    Refactored city data

--- internal/domain/city.go
+++ internal/domain/city.go
@@ -1,9 +1,33 @@
 package domain
 
 type City struct {
-	Name       string      `json:"name"`
-	Properties []*Property `json:"properties"`
+	Name   string      `json:"name"`
+	Prices *CityPrices `json:"prices"`
 }
+
+type CityPrices struct {
+	Restaurants    `json:"restaurants"`
+	Markets        `json:"markets"`
+	Transportation `json:"transportation"`
+	Utilities      `json:"utilities"`
+	Sports         `json:"sports"`
+	Childcare      `json:"childcare"`
+	Clothing       `json:"clothing"`
+	RentApartment  `json:"rentApartment"`
+	BuyApartment   `json:"buyApartment"`
+	Salaries       `json:"salaries"`
+}
+
+type Restaurants []*Property
+type Markets []*Property
+type Transportation []*Property
+type Utilities []*Property
+type Sports []*Property
+type Childcare []*Property
+type Clothing []*Property
+type RentApartment []*Property
+type BuyApartment []*Property
+type Salaries []*Property
 
 type Price struct {
 	Avg float64 `json:"avg"`
--- internal/infrastructure/numbeo/numbeo_test.go
+++ internal/infrastructure/numbeo/numbeo_test.go
@@ -5,10 +5,7 @@ import (
 	"testing"
 )
 
-const mealInexpensiveRestaurant = "Meal, Inexpensive Restaurant"
-const apartment1BedroomOutsideOfCenter = "Apartment (1 bedroom) Outside of Centre"
-const apartment3BedroomOutsideOfCenter = "Apartment (3 bedrooms) Outside of Centre"
-const mortgageInterestRate = "Mortgage Interest Rate in Percentages (%), Yearly, for 20 Years Fixed-Rate"
+const averageMonthlyNetSalary = "Average Monthly Net Salary (After Tax)"
 
 func TestGetCity(t *testing.T) {
 	t.Run("should return city info when city exists", func(t *testing.T) {
@@ -22,11 +19,16 @@ func TestGetCity(t *testing.T) {
 		if city.Name != cn {
 			t.Errorf("Name is unexpected: expected = %s, actual = %s\n", cn, city.Name)
 		}
-		props := getProperties(city)
-		checkProperty(t, mealInexpensiveRestaurant, props)
-		checkProperty(t, apartment1BedroomOutsideOfCenter, props)
-		checkProperty(t, apartment3BedroomOutsideOfCenter, props)
-		checkProperty(t, mortgageInterestRate, props)
+		checkPrices(t, city.Prices.Restaurants)
+		checkPrices(t, city.Prices.Markets)
+		checkPrices(t, city.Prices.Transportation)
+		checkPrices(t, city.Prices.Utilities)
+		checkPrices(t, city.Prices.Sports)
+		checkPrices(t, city.Prices.Childcare)
+		checkPrices(t, city.Prices.Clothing)
+		checkPrices(t, city.Prices.RentApartment)
+		checkPrices(t, city.Prices.BuyApartment)
+		checkPrices(t, city.Prices.Salaries)
 	})
 
 	t.Run("should return empty city when city not exists", func(t *testing.T) {
@@ -40,32 +42,28 @@ func TestGetCity(t *testing.T) {
 		if city.Name != cn {
 			t.Errorf("Name is unexpected: expected = %s, actual = %s\n", cn, city.Name)
 		}
-		if len(city.Properties) != 0 {
-			t.Errorf("City properties should be empty: properties = %v\n", city.Properties)
+		if city.Prices != nil {
+			t.Errorf("City prices should be nil: prices = %v\n", city.Prices)
 		}
 	})
 }
 
-func checkProperty(t *testing.T, name string, props map[string]*domain.Property) {
-	if p, ok := props[name]; !ok {
-		t.Errorf("Missed property: \"%s\"", name)
-	} else {
-		if p.Avg <= 0 {
-			t.Errorf("Avg should be > 0: property = %s, avg = %f\n", name, p.Avg)
-		}
-		if p.Min <= 0 {
-			t.Errorf("Min should be > 0: property = %s, min = %f\n", name, p.Min)
-		}
-		if p.Max <= 0 {
-			t.Errorf("Max should be > 0: property = %s, max = %f\n", name, p.Max)
-		}
+func checkPrices(t *testing.T, props []*domain.Property) {
+	for _, p := range props {
+		checkProperty(t, p)
 	}
 }
 
-func getProperties(c *domain.City) map[string]*domain.Property {
-	res := map[string]*domain.Property{}
-	for _, p := range c.Properties {
-		res[p.Name] = p
+func checkProperty(t *testing.T, p *domain.Property) {
+	if p.Avg <= 0 {
+		t.Errorf("Avg should be > 0: property = %+v\n", p)
 	}
-	return res
+	if p.Name != averageMonthlyNetSalary {
+		if p.Min <= 0 {
+			t.Errorf("Min should be > 0: property = %+v\n", p)
+		}
+		if p.Max <= 0 {
+			t.Errorf("Max should be > 0: property = %+v\n", p)
+		}
+	}
 }
